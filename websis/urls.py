fa26c077ccfe3ed61fb27832d9c607983141612b
shileen <shileen@outlook.com>
2016-06-07 19:29:32 +0530

    static

--- websis/urls.py
+++ websis/urls.py
@@ -10,7 +10,3 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
 
-if not settings.DEBUG:
-    urlpatterns += url('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
